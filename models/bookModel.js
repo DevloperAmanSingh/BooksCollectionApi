9c22f771c8af41ba15cb0c094e14a373001567d0
DevloperAmanSingh <80804989+DevloperAmanSingh@users.noreply.github.com>
2023-08-25 22:04:16 +0530

    Update bookModel.js

--- models/bookModel.js
+++ models/bookModel.js
@@ -7,6 +7,6 @@ const bookSchema = new mongoose.Schema({
     genre: {type: String, required: true},
     year: {type: Number, required: true},
     rating: {type: Number, required: true},    
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model('Book', bookSchema);
